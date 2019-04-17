d3924c75227962eda52bc7e148ebb37780e6389a
Peter deHaan <peter@deseloper.com>
2019-04-16 17:04:03 -0700

    v2

--- cli.js
+++ cli.js
@@ -5,13 +5,13 @@ const lib = require("./lib");
 main();
 
 async function main() {
-  const missingBreaches = await lib.getMissingBreaches();
-
-  if (missingBreaches.length) {
-    console.info("Missing:");
-    console.error(JSON.stringify(missingBreaches, null, 2));
+  try {
+    await lib.getMissingBreaches();
+    console.log("RemoteSettings is up to date!");
+  } catch (err) {
+    console.info(err.message);
+    console.error(JSON.stringify(err.missingBreaches, null, 2));
     process.exitCode = 1;
     return;
   }
-  console.log("RemoteSettings is up to date!");
 }
--- lib.js
+++ lib.js
@@ -1,23 +1,33 @@
 const axios = require("axios");
 
+const HIBP_BREACH_ENDPOINT = "https://monitor.firefox.com/hibp/breaches";
+const REMOTE_SETTINGS_ENDPOINT = "https://firefox.settings.services.mozilla.com/v1/buckets/main/collections/fxmonitor-breaches/records";
+
 module.exports = {
+  HIBP_BREACH_ENDPOINT,
+  REMOTE_SETTINGS_ENDPOINT,
   getMissingBreaches
 };
 
 async function getMissingBreaches() {
   const hibpBreaches = await fetchHIBP();
   const remoteSettingsBreaches = await fetchRemoteSettings();
-
-  return hibpBreaches.filter(breach => !remoteSettingsBreaches.has(breach.Name));
+  const missingBreaches = hibpBreaches.filter(breach => !remoteSettingsBreaches.has(breach.Name));
+  if (missingBreaches.length) {
+    const err = new Error("Missing breaches");
+    err.missingBreaches = missingBreaches;
+    throw err;
+  }
+  // return missingBreaches;
 }
 
 async function fetchHIBP() {
-  const res = await axios.get("https://monitor.firefox.com/hibp/breaches");
+  const res = await axios.get(HIBP_BREACH_ENDPOINT);
   return res.data.filter(breach => isActiveBreach(breach));
 }
 
 async function fetchRemoteSettings() {
-  const res = await axios.get("https://firefox.settings.services.mozilla.com/v1/buckets/main/collections/fxmonitor-breaches/records");
+  const res = await axios.get(REMOTE_SETTINGS_ENDPOINT);
   return new Set(res.data.data.map(breach => breach.Name));
 }
 
